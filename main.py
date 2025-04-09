5e015c36a037471e418f63978d9ed854f26379c9
Rebeca <rebeca.carmo.lopes@escola.pr.gov.br>
2025-04-09 08:16:52 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,22 +11,52 @@ def exibir_menu():
 
 def salvar_cadastros (cadastros):
     with open (arquivo_cadastros, "w", encoding="utf-8") as arquivo:
-             json.dump(cadastros, arquivo, ident=4,ensure_ascii=False)
+             json.dump(cadastros, arquivo, indent=4,ensure_ascii=False)
 
 def carregar_cadastros():
       try:
             with open (arquivo_cadastros, "r", encoding="utf-8") as arquivos:
                   return json.load(arquivos)
-      except(FileNotFoundError, json.JSONDecodeERROR):
+      except(FileNotFoundError, json.JSONDecodeError):
             return []
       
-def cadastrar_pessoa(cadastras):
+def cadastrar_pessoa(cadastros):
       nome= input("Nome: ")
-      nome= input("Idade: ")
-      nome= input("Turmas: ")
-      nome= input("Cursos: ")
+      idade= input("Idade: ")
+      turma= input("Turmas: ")
+      curso= input("Cursos: ")
 
-      cadastros.append("Nome": nome, "Idade": idade, "Turma":turma, "Cursos":curso)
+      cadastros.append({"Nome": nome, "Idade": idade, "Turma":turma, "Curso":curso})
       salvar_cadastros(cadastros)
       print("Cadastro realizado com sucesso!")
+def ver_cadastros (cadastros):
+      if not cadastros: 
+            print ("Cadastros realizados com sucesso!")
 
+def ver_cadastros(cadastros):
+    if not cadastros:
+        print("\nNenhum cadastro realizado")
+    else:
+        print("\n==== LISTA DE CADASTROS ====")
+        for i, pessoa in enumerate(cadastros, 1):
+            print(f"{i}. Nome: {pessoa['Nome']}, Idade: {pessoa['Idade']}, Turma: {pessoa['Turma']}, Curso: {pessoa['Cursos']}")
+    input("\nPressione Enter para voltar ao menu...")
+
+def main ():
+    cadastros = carregar_cadastros()
+    while True:
+        exibir_menu()
+        opcao = input("Escolha uma opção:  ")
+        if opcao == "1":
+             cadastrar_pessoa(cadastros)
+        elif opcao == "2":
+             ver_cadastros(cadastros)
+        elif opcao == "3":
+             print("Obrigada por utilizar o sistema de cadastro!")
+             break
+        else:
+            print("Opção inválida!Tente novamente.")
+         
+
+if __name__ == "__main__":
+    main()
